250f8648ac4cee29196de378e229a2dff0e12e1b
Martin <martinhorpacs@gmail.com>
2022-02-27 11:01:01 +0100

    rand fix

--- src/random.rs
+++ src/random.rs
@@ -18,9 +18,9 @@ fn gamm(x: f64) -> f64 {
     ret * f64::sqrt(2. * PI)/x * f64::powf(x + 5.5, x+0.5) * f64::exp(-x-5.5)
 }
 
-fn gammaPdf(x: f64, a: f64, b: f64) {
+fn gammaPdf(x: f64, a: f64, b: f64) -> f64 {
     assert!(x > 0. && a > 0. && b > 0.);
-    f64::exp(-x * b) * f64::powf(x, a - 1.0) * f64::powf(b, a) / gamm(a);
+    f64::exp(-x * b) * f64::powf(x, a - 1.0) * f64::powf(b, a) / gamm(a)
 }
 
 impl rand {
@@ -35,8 +35,8 @@ impl rand {
         self.z
     }
 
-    pub fn randGamma(&mut self, x: f64, a: f64, b: f64) {
-        gammaPdf(x * self.xorshf96() as f64 / usize::MAX as f64, a, b);
+    pub fn rand_gamma(&mut self, x: f64, a: f64, b: f64) -> f64 {
+        gammaPdf(x * self.xorshf96() as f64 / usize::MAX as f64, a, b)
     }
 
     pub fn new() -> rand {
