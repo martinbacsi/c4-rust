a90d0abfe74cf925f63861b4b456e11458aaa9f6
Martin <martinhorpacs@gmail.com>
2022-02-28 19:59:23 +0100

    cg play

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,30 +1,36 @@
-use crate::NNManager;
-use crate::Pool;
-use crate::Node;
-use crate::POLICY_SIZE;
 use crate::conf;
 use crate::connect4::Outcome;
 use crate::nn::NN;
 use crate::random::dirichlet_noise;
 use crate::random::rand_float;
+use crate::NNManager;
+use crate::Node;
+use crate::Pool;
+use crate::POLICY_SIZE;
+use std::io;
 use std::time::Duration;
 use std::{collections::HashMap, mem, time::Instant};
-use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
 pub struct MCTS {
     pool: Pool,
     root: Box<Node>,
-    nn: NNManager
+    nn: NNManager,
 }
 
 impl MCTS {
     pub fn new() -> MCTS {
         let mut mcts = MCTS {
             pool: Pool::new(1000000),
-            root: Box::new(Node::new()), 
-            nn: NNManager{
+            root: Box::new(Node::new()),
+            nn: NNManager {
                 cache: HashMap::new(),
-                nn: NN::new()
-            }
+                nn: NN::new(),
+            },
         };
         mcts.nn.nn.read_weights("best.w32");
         mcts
@@ -57,19 +63,21 @@ impl MCTS {
             if d > best {
                 best = d;
                 a = c.game.last_move;
-            } 
+            }
         });
         (a, p)
     }
 
-    fn get_move_probs_play(&mut self,  endt: Instant) -> (u8, [f64; POLICY_SIZE]) {
+    fn get_move_probs_play(&mut self, endt: Instant) -> (u8, [f64; POLICY_SIZE]) {
         while Instant::now() < endt {
             self.root.playout(&mut self.nn, &mut self.pool);
         }
         let p = self.root.prob_vector();
         let mut a = self.root.children[0].game.last_move;
-        self.root.children.iter().for_each(|c|{
-            if p[c.game.last_move as usize] > p[self.root.children[a as usize].game.last_move as usize] {
+        self.root.children.iter().for_each(|c| {
+            if p[c.game.last_move as usize]
+                > p[self.root.children[a as usize].game.last_move as usize]
+            {
                 a = c.game.last_move;
             }
         });
@@ -85,11 +93,11 @@ impl MCTS {
 
     pub fn play_against(&mut self) {
         while self.root.game.outcome == Outcome::None {
-            let (a, _) = self.get_move_probs_play( Instant::now() + Duration::from_millis(1000));
+            let (a, _) = self.get_move_probs_play(Instant::now() + Duration::from_millis(1000));
             self.update_with_action(a);
 
             if self.root.game.outcome != Outcome::None {
-                print!("LOL XD");
+                println!("LOL XD");
                 break;
             }
 
@@ -100,5 +108,29 @@ impl MCTS {
             self.update_with_action(n);
         }
         self.root.game.print();
+    }
+
+    pub fn cg(&mut self) {
+        let mut endt = Instant::now() + Duration::from_millis(900);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        self.root.playout(&mut self.nn, &mut self.pool);
+        for i in 0..64 {
+            if i > 0 {
+                endt = Instant::now() + Duration::from_millis(100);
+            }
+            for _i in 0..10 + self.root.children.len() as usize {
+                io::stdin().read_line(&mut input_line).unwrap();
+            }
+            let opp_previous_action = parse_input!(input_line, i32); // opponent's previous chosen column index (will be -1 for first player in the first turn)
+
+            if opp_previous_action >= 0 {
+                self.update_with_action(opp_previous_action as u8);
+            }
+
+            let (a, _p) = self.get_move_probs_play(endt);
+            self.update_with_action(a);
+            println!("{}", a);
+        }
     }
 }
